32f154510c1d152786a697c9029945eccec45e72
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-15 00:40:49 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser') // 引用 body-parser
 const port = 3000
 
-const Todo = require('./models/todo') // 載入 Todo model
+const Todo = require('./models/todo')
 
 const app = express()
 
@@ -22,12 +23,31 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.log(error)) // 錯誤處理
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.log(error))
 })
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  // return Todo.create({ name })
+  //   .then(() => res.redirect('/'))
+  //   .catch(error => console.log('error'))
+  // 另一種寫法
+  const todo = new Todo({ name })
+  return todo.save()
+    .then(() => res.redirect('/'))
+    .catch(error => console.log('error'))
+})
+
 
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000')
