74928dcc7c240e1b2e22f55216ef3a3419bba9f1
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-14 19:17:54 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-// 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const port = 3000
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 取得資料庫連線狀態
 const db = mongoose.connection
-// 連線異常
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
+
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 設定首頁路由
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
-// 設定 port 3000
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('App is running on http://localhost:3000')
 })
