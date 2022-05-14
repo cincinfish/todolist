2915a31f5be4afdb385116d7376a5028a16d5cc3
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-14 23:34:00 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const port = 3000
 
+const Todo = require('./models/todo') // 載入 Todo model
+
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,7 +23,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.log(error)) // 錯誤處理
 })
 
 app.listen(port, () => {
