5e9d2870040e143188384d6515b7f682feddb8c6
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-23 16:29:35 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,91 +1,20 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') // 引用 body-parser
 const port = 3000
-
-const Todo = require('./models/todo')
 const methodOverride = require('method-override')
+
 const routes = require('./routes') // 引用路由器
+require('./config/mongoose')
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 app.use(routes)
-
-// app.get('/', (req, res) => {
-//   Todo.find()
-//     .lean()
-//     .sort({ _id: 'asc' }) //反序desc
-//     .then(todos => res.render('index', { todos }))
-//     .catch(error => console.log(error))
-// })
-
-// app.get('/todos/new', (req, res) => {
-//   return res.render('new')
-// })
-
-// app.get('/todos/:id', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .lean()
-//     .then((todo) => res.render('detail', { todo }))
-//     .catch(error => console.log('error'))
-// })
-
-// app.get('/todos/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .lean()
-//     .then((todo) => res.render('edit', { todo }))
-//     .catch(error => console.log(error))
-// })
-
-// app.put('/todos/:id', (req, res) => {
-//   const id = req.params.id
-//   const { name, isDone } = req.body
-//   return Todo.findById(id)
-//     .then(todo => {
-//       todo.name = name
-//       todo.isDone = isDone === 'on'
-//       return todo.save()
-//     })
-//     .then(() => res.redirect(`/todos/${id}`))
-//     .catch(error => console.log(error))
-// })
-
-// app.post('/todos', (req, res) => {
-//   const name = req.body.name
-//   const todo = new Todo({ name })
-//   return todo.save()
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log('error'))
-// })
-
-// app.delete('/todos/:id', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .then(todo => todo.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
 
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000')
