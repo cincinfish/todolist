426bedcfa7076a22603d310695c0898800d99591
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-23 12:41:17 +0800

    feat: model to add isDone params

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser') // 引用 body-parser
 const port = 3000
 
 const Todo = require('./models/todo')
+const todo = require('./models/todo')
 
 const app = express()
 
@@ -49,7 +50,20 @@ app.get('/todos/:id/edit', (req, res) => {
   return Todo.findById(id)
     .lean()
     .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log('error'))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
 })
 
 app.post('/todos', (req, res) => {
@@ -60,18 +74,6 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log('error'))
 })
 
-app.post('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
 app.post('/todos/:id/delete', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -79,7 +81,6 @@ app.post('/todos/:id/delete', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-
 
 
 app.listen(port, () => {
