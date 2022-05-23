f82ccdc27bf740ba9f721942cfaf66ece05933b6
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-23 16:10:42 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000
 
 const Todo = require('./models/todo')
 const methodOverride = require('method-override')
+const routes = require('./routes') // 引用路由器
 
 const app = express()
 
@@ -26,63 +27,64 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .sort({ _id: 'asc' }) //反序desc
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.log(error))
-})
+// app.get('/', (req, res) => {
+//   Todo.find()
+//     .lean()
+//     .sort({ _id: 'asc' }) //反序desc
+//     .then(todos => res.render('index', { todos }))
+//     .catch(error => console.log(error))
+// })
 
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
+// app.get('/todos/new', (req, res) => {
+//   return res.render('new')
+// })
 
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log('error'))
-})
+// app.get('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('detail', { todo }))
+//     .catch(error => console.log('error'))
+// })
 
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
+// app.get('/todos/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('edit', { todo }))
+//     .catch(error => console.log(error))
+// })
 
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
+// app.put('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   const { name, isDone } = req.body
+//   return Todo.findById(id)
+//     .then(todo => {
+//       todo.name = name
+//       todo.isDone = isDone === 'on'
+//       return todo.save()
+//     })
+//     .then(() => res.redirect(`/todos/${id}`))
+//     .catch(error => console.log(error))
+// })
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  const todo = new Todo({ name })
-  return todo.save()
-    .then(() => res.redirect('/'))
-    .catch(error => console.log('error'))
-})
+// app.post('/todos', (req, res) => {
+//   const name = req.body.name
+//   const todo = new Todo({ name })
+//   return todo.save()
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log('error'))
+// })
 
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// app.delete('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .then(todo => todo.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
 
 app.listen(port, () => {
