de6cb3b7b27b373a3f0fe888e840eb9bf3c1feae
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-12 12:30:09 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') // 引用 body-parser
 const port = process.env.PORT || 3000
 const methodOverride = require('method-override')
+const session = require("express-session")
+require('dotenv').config();
 
 const routes = require('./routes') // 引用路由器
 require('./config/mongoose')
@@ -13,7 +15,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
 
 app.listen(port, () => {
