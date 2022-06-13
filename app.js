ed9135ad1f46c87284aa406bf057c1e64b55c343
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-14 00:37:23 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') // 引用 body-parser
 const port = process.env.PORT || 3000
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const session = require("express-session")
 
 require('dotenv').config()
@@ -22,6 +23,16 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.login_msg = req.flash('login_msg')
+  next()
+})
+
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 app.use((req, res, next) => {
