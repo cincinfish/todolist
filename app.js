b825faffe3f6f3efd25ea9fca32904351756d2e7
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-17 14:18:20 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require("express-session")
 
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes') // 引用路由器
 // 載入設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport')
@@ -19,7 +22,7 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
 
-// 引入路由模組
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
-const { authenticator } = require('../middleware/auth')
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/todos', authenticator, todos) // 加入驗證程序
-router.use('/users', users)
-router.use('/', authenticator, home) // 加入驗證程序
+const auth = require('./modules/auth')   
 
-// 匯出路由器
+const { authenticator } = require('../middleware/auth')
+
+router.use('/todos', authenticator, todos)
+router.use('/users', users)
+router.use('/auth', auth)  
+router.use('/', authenticator, home)
+
 module.exports = router
